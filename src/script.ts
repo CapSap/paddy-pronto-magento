ce5f62448a4f91a253042daba4e6ed3d234b6c43
CapSap <cshotam@gmail.com>
2024-02-15 12:29:21 +1100

    add in a fucction that checks if DOM is fully rendered

--- src/script.ts
+++ src/script.ts
@@ -41,6 +41,46 @@ import * as fs from "node:fs";
       await prontoPage.keyboard.press("Enter");
     }
   }
+  // from https://stackoverflow.com/questions/52497252/puppeteer-wait-until-page-is-completely-loaded
+  // waitForNavigation and waitForNetworkIdle didnt work
+  const waitTillHTMLRendered = async (page: Page, timeout = 30000) => {
+    const checkDurationMsecs = 1000;
+    const maxChecks = timeout / checkDurationMsecs;
+    let lastHTMLSize = 0;
+    let checkCounts = 1;
+    let countStableSizeIterations = 0;
+    const minStableSizeIterations = 3;
+
+    while (checkCounts++ <= maxChecks) {
+      const html = await page.content();
+      const currentHTMLSize = html.length;
+
+      const bodyHTMLSize = await page.evaluate(
+        () => document.body.innerHTML.length,
+      );
+
+      console.log(
+        "last: ",
+        lastHTMLSize,
+        " <> curr: ",
+        currentHTMLSize,
+        " body html size: ",
+        bodyHTMLSize,
+      );
+
+      if (lastHTMLSize != 0 && currentHTMLSize == lastHTMLSize)
+        countStableSizeIterations++;
+      else countStableSizeIterations = 0; //reset the counter
+
+      if (countStableSizeIterations >= minStableSizeIterations) {
+        console.log("Page rendered fully..");
+        break;
+      }
+
+      lastHTMLSize = currentHTMLSize;
+      await page.waitForTimeout(checkDurationMsecs);
+    }
+  };
 
   const runAsyncFuncInSeries = async (array, fun: (order: object) => void) => {
     const results = [];
