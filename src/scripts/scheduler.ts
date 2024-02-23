93c93a7e132ab9d062f86d46010dc6212c8e6f34
CapSap <cshotam@gmail.com>
2024-02-23 16:27:51 +1100

    save wip

--- src/scripts/scheduler.ts
+++ src/scripts/scheduler.ts
@@ -2,14 +2,26 @@ import { prontoSellMagCommentScript } from "../functions/main.js";
 
 const minutes = process.argv.slice(2)[0];
 
-async function scheduler(minutes: number) {
+function scheduler(minutes: number) {
   const milliseconds = minutes * 60000;
 
   setInterval(async () => {
     console.log("Script starting at", new Date().toLocaleTimeString());
     // await prontoSellMagCommentScript();
+    await someAsyncTask();
+    await delay(22);
     console.log("Script complete at ", new Date().toLocaleTimeString());
   }, milliseconds);
+}
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function someAsyncTask() {
+  console.log("is this running");
+  setTimeout(() => {
+    console.log("running at ", new Date().toLocaleTimeString());
+  }, 20);
 }
 
 async function main(minutes) {
@@ -19,11 +31,13 @@ async function main(minutes) {
     // if minutes is undefined, run a default 30 mins
   } else if (minutes === undefined) {
     console.log("No minute argument passed. Running every 30 mins by default");
-    await scheduler(30);
+    scheduler(30);
   } else {
     // and if a number is passed, run the func ever number of minutes
-    await scheduler(minutes);
+    scheduler(minutes);
   }
 }
 
-main(minutes);
+// main(minutes);
+
+scheduler(0.1);
