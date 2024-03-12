b318a8737b99b0bc3c6abe0f0b2ae959c25b8a23
CapSap <cshotam@gmail.com>
2024-03-12 14:36:55 +1100

    change to every 2 hours

--- src/scripts/scheduler.ts
+++ src/scripts/scheduler.ts
@@ -1,12 +1,13 @@
 import { prontoSellMagCommentScript } from "../functions/prontoSellMagCommentScript.js";
 
 console.log(
-  "Pronto sell and magento comment script running every 30 mins. Starting at",
+  "Pronto sell and magento comment script running every 2 hrs. Starting at",
   new Date().toLocaleTimeString(),
 );
 // 1800000 milliseonds = 30 mins
+// 7200000 milliseconds = 2 hours
 setInterval(async () => {
   console.log("Script starting at", new Date().toLocaleTimeString());
   await prontoSellMagCommentScript();
   console.log("Script complete at ", new Date().toLocaleTimeString());
-}, 1800000);
+}, 7200000);
